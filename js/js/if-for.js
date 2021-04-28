d7bf61f25e4838fc6d4dfad6b15d99e0f5bb8b06
Ami-Sato5 <ami.sato@force-corp.co.jp>
2021-04-28 18:36:09 +0900

    test2

---
+++ js/js/if-for.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// let i = 2;
+// if(i === 1){
+//     console.log("真です");
+// }
+
+// let testscore = 80;
+
+// if(testscore >= 80) {
+//     document.write("合格です！おめでとうございます！");
+
+// } else if (testscore >= 70) {
+//     document.write("惜しい！あと少し！");
+
+// } else {
+//     document.write("不合格です！もう少し勉強しましょう");
+// }
+
+const nameList = ["田中","山田","林"];
+
+for (let i = 0; i < nameList.length ; i++) {
+    console.log(nameList[i]);
+}
